b4fc06f8545dca33dbf0025814623f0c28c72a4d
tintrandn <tintrandn@gmail.com>
2024-06-15 16:28:48 +0800

    Init commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const userRouter = require('./routers/users')
+const taskRouter = require('./routers/tasks')
+
+require('./db/mongoose')
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+
+
+
